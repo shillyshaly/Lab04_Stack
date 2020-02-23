e099ddd5659c68f47f49069e2b1677d58b52bb50
Jamie Hernandez <litoxl@hotmail.com>
2020-02-22 19:42:20 -0800

    EXCELSIOR!!!

--- src/TestArea.java
+++ src/TestArea.java
@@ -4,16 +4,6 @@ import java.util.Stack;
 public class TestArea {
     public static void main (String[] args){
 
-        LinkedStack<String> myStack = new LinkedStack<>();
-        myStack.push("A");
-        myStack.push("B");
-        myStack.push("C");
-        myStack.push("D");
-        myStack.push("E");
-        myStack.displayStack();
-        myStack.remove(2);
-        System.out.println();
-        myStack.displayStack();
 
     }
 }
